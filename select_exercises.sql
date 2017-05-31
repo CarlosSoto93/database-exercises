d542170cddf514993422c6628b3c2698ad484df1
Carlos <c.soto93@hotmail.com>
2017-05-31 15:37:59 -0500

    Select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT name AS 'Pink Floyd''s albums' FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT  release_date AS 'Sgt. Pepper''s Lonely Hearts Club Band release year' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre AS 'The Genre of Nevermined' FROM albums WHERE name = 'Nevermind';
+
+SELECT name AS 'Albums released in the 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999 ;
+
+SELECT name AS 'Albums w/ less than 20 mill. in sales' FROM albums WHERE sales <= 20;
+
+SELECT name AS 'Albums w/ the rock genre' FROM albums WHERE genre = 'rock';
