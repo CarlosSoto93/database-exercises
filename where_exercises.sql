3deb38ac16532d080ab7aae43330bccd163a0706
Carlos <c.soto93@hotmail.com>
2017-06-01 15:34:40 -0500

    Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,13 +5,18 @@ FROM employees
 WHERE gender = 'M'
       AND (first_name = 'Irena'
            OR first_name = 'Vidya'
-           OR first_name = 'Maya')
-);
+           OR first_name = 'Maya'
+      );
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
       OR last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
 
 SELECT *
 FROM employees
