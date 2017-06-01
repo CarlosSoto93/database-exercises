8fa81ac2a89e01f55f37d2de237f5f164d3a9c4e
Carlos <c.soto93@hotmail.com>
2017-06-01 10:24:17 -0500

    deleted selection from the database.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
 SELECT 'Albums released after 1991' AS'';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'Albums with the genre Disco' AS'';
-SELECT * FROM albums WHERE genre LIKE '%disco%';
+DELETE FROM albums WHERE genre LIKE '%disco%';
 
 SELECT 'Albums by ''Whitney Houston''' AS'';
-SELECT * FROM albums WHERE artist LIKE 'Whitney Houston%';
+DELETE FROM albums WHERE artist LIKE 'Whitney Houston%';
