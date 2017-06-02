32f402fa01a235ca72bfd2648e37c58ed2563902
Carlos <c.soto93@hotmail.com>
2017-06-02 09:34:58 -0500

    Updated previous query to now find unique combinations of first and last name.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,8 +4,8 @@ SELECT DISTINCT title
 FROM titles
 GROUP BY title ASC;
 
-SELECT DISTINCT last_name
+SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%e'
-GROUP BY last_name ASC;
+GROUP BY first_name ,last_name ASC;
