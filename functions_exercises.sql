f013080ccbb741db9f34b2c43b6aa8385e79954e
Carlos <c.soto93@hotmail.com>
2017-06-02 12:09:44 -0500

    For the query of employees born on Christmas and hired in the 90s, I use datediff() to find how many days they have been working at the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -16,7 +16,13 @@ FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%e';
 
-SELECT *
+SELECT concat(
+    first_name,
+    ' ',
+    last_name,
+    ' has been working at the company for ',
+    (datediff(curdate(), hire_date)),
+  ' days')
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
       AND birth_date LIKE '%12-25'
