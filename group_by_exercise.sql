b6004513929cd7aec1e0113df6668e1b8b92c3f5
Carlos <c.soto93@hotmail.com>
2017-06-02 10:32:22 -0500

    Created a new SQL script called limit_exercises.sql

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%e'
-GROUP BY first_name ,last_name ASC;
+GROUP BY last_name ASC;
 
 SELECT DISTINCT last_name
 FROM employees
