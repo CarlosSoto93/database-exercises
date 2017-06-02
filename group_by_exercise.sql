0c09bd4d89fe9242619d3cb523a00a0a20043025
Carlos <c.soto93@hotmail.com>
2017-06-02 09:32:09 -0500

    Updated past query for employees start and end with 'e' to fined the unique names ascending using GROUP BY.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -3,3 +3,9 @@ USE employees;
 SELECT DISTINCT title
 FROM titles
 GROUP BY title ASC;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e'
+GROUP BY last_name ASC;
