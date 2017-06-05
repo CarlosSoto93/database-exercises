a18588f6eef02fa1b1d886be16ec092ed7ca8ea1
Carlos <c.soto93@hotmail.com>
2017-06-05 11:00:21 -0500

    Found the current titles of employees currently working in the Customer Service department.

--- employees_database_exercise.sql
+++ employees_database_exercise.sql
@@ -19,3 +19,15 @@ JOIN dept_manager AS dm ON d.dept_no=dm.dept_no
 JOIN employees AS e ON dm.emp_no=e.emp_no
 WHERE dm.to_date > now() AND e.gender = 'F'
 ORDER BY d.dept_name;
+
+# Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title AS Title, COUNT(*) AS Count
+FROM titles AS t
+JOIN employees AS e ON t.emp_no = e.emp_no
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE t.to_date > now()
+  AND de.to_date > now()
+  AND d.dept_name = 'Customer Service'
+GROUP BY t.title;
+
