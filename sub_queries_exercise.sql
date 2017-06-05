46b511c66e5c586daa166ab42eeff54802e69e4e
Carlos <c.soto93@hotmail.com>
2017-06-05 11:49:58 -0500

    Found all the department managers that are female.

--- sub_queries_exercise.sql
+++ sub_queries_exercise.sql
@@ -13,14 +13,24 @@ USE employees;
 # );
 
 # Find all the titles held by all employees with the first name Aamod.
-SELECT title
-FROM titles
-WHERE emp_no IN (
-  SELECT emp_no
-  FROM employees
-  WHERE first_name = 'Aamod'
-)
-GROUP BY title;
+# SELECT title
+# FROM titles
+# WHERE emp_no IN (
+#   SELECT emp_no
+#   FROM employees
+#   WHERE first_name = 'Aamod'
+# )
+# GROUP BY title;
 
 # Find all the department managers that are female.
+SELECT
+  CONCAT(first_name, ' ', last_name) AS 'Department Managers',
+  gender
+FROM employees
+WHERE gender = 'F'
+AND emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+);
+
 # BONUS Find all the department names that have female managers.
