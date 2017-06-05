5cfdb92514e2531824763a1895f3cc53cca0a7ac
Carlos <c.soto93@hotmail.com>
2017-06-05 11:44:13 -0500

    Found all the titles held by all employees with the first name Aamod.

--- sub_queries_exercise.sql
+++ sub_queries_exercise.sql
@@ -1,17 +1,26 @@
 USE employees;
 
 # Find all the employees with the same hire date as employee 101010 using a sub-query.
-SELECT
-  emp_no,
-  CONCAT(first_name, ' ', last_name) AS Employee,
-  hire_date
-FROM employees
-WHERE hire_date IN (
-  SELECT hire_date
-  FROM employees
-  WHERE emp_no = 101010
-);
+# SELECT
+#   emp_no,
+#   CONCAT(first_name, ' ', last_name) AS Employee,
+#   hire_date
+# FROM employees
+# WHERE hire_date IN (
+#   SELECT hire_date
+#   FROM employees
+#   WHERE emp_no = 101010
+# );
 
 # Find all the titles held by all employees with the first name Aamod.
+SELECT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+)
+GROUP BY title;
+
 # Find all the department managers that are female.
 # BONUS Find all the department names that have female managers.
