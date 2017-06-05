e6676efd97321395155f7d037f3ecf0bc54a78c9
Carlos <c.soto93@hotmail.com>
2017-06-05 10:28:18 -0500

    Found the name of all departments currently managed by women.

--- employees_database_exercise.sql
+++ employees_database_exercise.sql
@@ -1,8 +1,21 @@
 USE employees;
 
-SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS 'full name'
+# write a query that shows each department along with the name of the current manager for that department.
+SELECT
+  d.dept_name AS 'Department Name',
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Department Name'
 FROM departments AS d
 JOIN dept_manager AS dm ON d.dept_no=dm.dept_no
 JOIN employees AS e ON dm.emp_no=e.emp_no
-WHERE dm.to_date LIKE '9999%'
+WHERE dm.to_date > now()
+ORDER BY d.dept_name;
+
+# Find the name of all departments currently managed by women.
+SELECT
+  d.dept_name AS 'Department Name',
+  CONCAT(e.first_name, ' ', e.last_name) AS  'Manager Name'
+FROM departments AS d
+JOIN dept_manager AS dm ON d.dept_no=dm.dept_no
+JOIN employees AS e ON dm.emp_no=e.emp_no
+WHERE dm.to_date > now() AND e.gender = 'F'
 ORDER BY d.dept_name;
